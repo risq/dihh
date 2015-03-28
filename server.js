45f14a6753e6420b6bd8f974be5666fa724c94f2
risq <valentin.ledrapier@gmail.com>
2015-03-28 15:04:23 +0100

    Add bower, gulp and front end basic structure

--- server.js
+++ server.js
@@ -36,6 +36,9 @@ app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
+// app.use('/public', express.static(__dirname + '/public/dist'));
+app.use(express.static(__dirname + '/public/dist'));
+
 // routes ======================================================================
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
