a351a22ed707fb6fe0f28a5ba4e1f703f8bf8016
risq <valentin.ledrapier@gmail.com>
2015-07-17 00:23:28 +0200

    Move views directory

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.use(bodyParser.json()); // get information from html forms
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs'); // set up ejs for templating
+app.set('views', __dirname + '/client/views');
 
 // required for passport
 app.use(session({ 
