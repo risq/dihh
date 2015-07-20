f08b15204a5b54fd20965aa3ffb530fd395cd919
risq <valentin.ledrapier@gmail.com>
2015-07-20 20:48:52 +0200

    Truncate tweet is message is too long

--- app/twitter.js
+++ app/twitter.js
@@ -22,12 +22,15 @@ module.exports = {
 
 	},
 
-	tweet: function(message) {
+	tweet: function(message, url) {
 
 		if ( client ) {
+
+			var formattedMessage = url ? this.truncate(message, 115) + ' ' + url :
+								   this.truncate(message, 140);
 			
 			client.post('statuses/update', {
-				status: message
+				status: formattedMessage
 			}, function(error, tweet, response){
 
 				if ( error ) {
@@ -40,7 +43,31 @@ module.exports = {
 	},
 
 	tweetDig: function(dig) {
-		this.tweet(dig.artists.join(', ') + ' - ' + dig.title + ' (' + dig.year + ') ' + shareUrl + '/digs/' + dig.slug);
+		var message = dig.artists.join(', ') + ' - ' + dig.title + ' (' + dig.year + ')',
+			url = shareUrl + '/digs/' + dig.slug;
+		this.tweet(message, url);
+	},
+
+	// http://snipplr.com/view/16108/truncate-a-string-to-a-set-length-breaking-at-word-boundaries/
+	truncate: function (str, limit) {
+	    var bits, i;
+	    if ('string' !== typeof str) {
+	        return '';
+	    }
+	    bits = str.split('');
+	    if (bits.length > limit) {
+	        for (i = bits.length - 1; i > -1; --i) {
+	            if (i > limit - 4) {
+	                bits.length = i;
+	            }
+	            else if (' ' === bits[i]) {
+	                bits.length = i;
+	                break;
+	            }
+	        }
+	        bits.push('...');
+	    }
+	    return bits.join('');
 	}
 
 };
